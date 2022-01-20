d848d21089e4b66dab0e705bad065dc76746dac4
Nitin <nsinghboss02@gmail.com>
2022-01-20 23:10:12 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,28 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	print the prime numbers:
+	 */
+        int n=20;
+        for(int i=1;i<n;i++){
+            System.out.println(i+" "+isPrime(i));
+        }
+    }
+    public static boolean isPrime(int n){
+        if(n<=1){
+            return false;
+        }
+        if(n==2){
+            return true;
+        }
+        for(int i=2;i<=Math.sqrt(n);i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
